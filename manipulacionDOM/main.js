00c9d31d52f0ca3a1ff8c01da3dece99a27edc7c
antony24 <achuquival@gmail.com>
2022-08-24 17:43:50 -0500

    avance del la parte de hoy

--- manipulacionDOM/main.js
+++ manipulacionDOM/main.js
@@ -2,11 +2,13 @@ const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+// btn.addEventListener('click',btnOnClick);
 
 
-// escuchar eventos
-function btnOnClick(){
-    const pResult = document.querySelector('#result');
-    const sumaImput = parseInt(input1.value) + parseInt(input2.value);
-    pResult.innerText = `Resultado: ${sumaImput}`;
-}
+// // escuchar eventos
+// function btnOnClick(){
+//     const sumaImput = parseInt(input1.value) + parseInt(input2.value);
+//     pResult.innerText = `Resultado: ${sumaImput}`;
+// }
